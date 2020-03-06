97923d3a57863857f6e035645a0b97f828ba4b48
Michael Bikovitsky <biko@bikodbg.com>
2020-03-06 12:17:14 +0200

    Implement command-line parsing

--- littlefs-extract/main.cpp
+++ littlefs-extract/main.cpp
@@ -1,7 +1,86 @@
 #include <iostream>
+#include <string>
+#include <locale>
+#include <exception>
+#include <optional>
 
-int main()
+#include <boost/program_options.hpp>
+
+#include <lfs.h>
+
+
+namespace po = boost::program_options;
+
+
+struct CommandLineOptions
 {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    lfs_size_t block_size;
+    lfs_size_t read_size;
+    lfs_size_t prog_size;
+    lfs_size_t filesystem_size;
+    lfs_size_t file_offset;
+    std::wstring input_file_path;
+    std::wstring output_file_path;
+};
+
+
+#if defined(_MSC_VER)
+std::optional<CommandLineOptions> parse_command_line(int argc, wchar_t ** argv)
+#else
+std::optional<CommandLineOptions> parse_command_line(int argc, char ** argv)
+#endif
+{
+    CommandLineOptions options {};
+
+    try
+    {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("block-size,b", po::value(&options.block_size)->required(), "filesystem block size")
+            ("read-size,r", po::value(&options.read_size)->required(), "filesystem read size")
+            ("prog-size,p", po::value(&options.prog_size)->required(), "filesystem prog size")
+            ("filesystem-size",
+             po::value(&options.filesystem_size)->default_value(0),
+             "filesystem size (default is input file size)")
+            ("file-offset",
+             po::value(&options.file_offset)->default_value(0),
+             "offset within the input file where filesystem begins")
+            ("input-file,i", po::wvalue(&options.input_file_path)->required(), "littlefs image file")
+            ("output-file,o", po::wvalue(&options.output_file_path)->required(), "output tar file")
+        ;
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << desc << "\n";
+            return {};
+        }
+
+        po::notify(vm);
+    }
+    catch (std::exception const & exception)
+    {
+        std::cerr << exception.what() << "\n";
+        return {};
+    }
+
+    return options;
+}
+
+#if defined(_MSC_VER)
+int wmain(int argc, wchar_t ** argv)
+#else
+int main(int argc, char ** argv)
+#endif
+{
+    std::locale::global(std::locale(""));
+
+    auto const options = parse_command_line(argc, argv);
+    if (!options)
+    {
+        return 1;
+    }
 }
