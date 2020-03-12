dc7e1daf1930273628522fba56391cf3d11d1448
Michael Bikovitsky <biko@bikodbg.com>
2020-03-12 16:40:21 +0200

    Switch to using docopt

--- littlefs-extract/main.cpp
+++ littlefs-extract/main.cpp
@@ -1,16 +1,13 @@
 #include <cstdint>
 #include <exception>
 #include <iostream>
-#include <locale>
 #include <optional>
 #include <string>
 
-#include <boost/program_options.hpp>
+#include <docopt/docopt.h>
 
 #include <lfs1.h>
 #include <lfs2.h>
-
-namespace po = boost::program_options;
 
 struct CommandLineOptions
 {
@@ -24,60 +21,38 @@ struct CommandLineOptions
     std::wstring output_file_path;
 };
 
-#if defined(_MSC_VER)
-std::optional<CommandLineOptions> parse_command_line(int argc, wchar_t ** argv)
-#else
+static constexpr char USAGE[] =
+    R"(littlefs-extract.
+
+Usage:
+  littlefs-extract -l VERSION -b BLOCK_SIZE -r READ_SIZE -p PROG_SIZE [--filesystem-size FS_SIZE] [--file-offset OFFSET] -i INPUT [-o OUTPUT_FILE]
+  littlefs-extract -h | --help
+  littlefs-extract -h | --version
+
+Options:
+  -h --help                              Show this screen.
+  -v --version                           Show version.
+  -l VERSION --littlefs-version=VERSION  littlefs version to use.
+  -b BLOCK_SIZE --block-size=BLOCK_SIZE  filesystem block size.
+  -r READ_SIZE --read-size=READ_SIZE     filesystem read size.
+  -p PROG_SIZE --prog-size=PROG_SIZE     filesystem prog size.
+  --filesystem-size=FS_SIZE              filesystem size [default: 0].
+  --file-offset=OFFSET                   offset within the input file where filesystem begins.
+  -i INPUT --input-file=INPUT            littlefs image file.
+  -o OUTPUT --output-file=OUTPUT         output tar file [default: -].
+)";
+
 std::optional<CommandLineOptions> parse_command_line(int argc, char ** argv)
-#endif
 {
     CommandLineOptions options {};
 
-    try
+    auto args = docopt::docopt(USAGE,
+                               { argv + 1, argv + argc },
+                               true,
+                               "littlefs-extract 0.1");
+    for (auto const & argument : args)
     {
-        po::options_description desc("Allowed options");
-        desc.add_options()
-            ("help", "produce help message")
-            ("littlefs-version,l",
-                    po::value(&options.version)->required(),
-                    "littlefs version to use")
-            ("block-size,b",
-                    po::value(&options.block_size)->required(),
-                    "filesystem block size")
-            ("read-size,r",
-                    po::value(&options.read_size)->required(),
-                    "filesystem read size")
-            ("prog-size,p",
-                    po::value(&options.prog_size)->required(),
-                    "filesystem prog size")
-            ("filesystem-size",
-                    po::value(&options.filesystem_size)->default_value(0),
-                    "filesystem size (default is input file size)")
-            ("file-offset",
-                    po::value(&options.file_offset)->default_value(0),
-                    "offset within the input file where filesystem begins")
-            ("input-file,i",
-                    po::wvalue(&options.input_file_path)->required(),
-                    "littlefs image file")
-            ("output-file,o",
-                    po::wvalue(&options.output_file_path)->required(),
-                    "output tar file")
-        ;
-
-        po::variables_map vm;
-        po::store(po::parse_command_line(argc, argv, desc), vm);
-
-        if (vm.count("help"))
-        {
-            std::cout << desc << "\n";
-            return {};
-        }
-
-        po::notify(vm);
-    }
-    catch (std::exception const & exception)
-    {
-        std::cerr << exception.what() << "\n";
-        return {};
+        std::cout << argument.first << argument.second << "\n";
     }
 
     return options;
@@ -89,7 +64,7 @@ int wmain(int argc, wchar_t ** argv)
 int main(int argc, char ** argv)
 #endif
 {
-    std::locale::global(std::locale(""));
+    // TODO: convert command-line to UTF-8
 
     auto const options = parse_command_line(argc, argv);
     if (!options)
