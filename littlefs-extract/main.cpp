0933ebd23012de7a90d2c0ec1015cc8026fac667
Michael Bikovitsky <biko@bikodbg.com>
2020-03-13 17:56:46 +0200

    Switch back to boost::program_options

--- littlefs-extract/main.cpp
+++ littlefs-extract/main.cpp
@@ -7,9 +7,9 @@
 #include <utility>
 #include <stdexcept>
 #include <limits>
+#include <optional>
 
-#include <docopt/docopt.h>
-#include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
 
 #include "Util.hpp"
 #include "FileBlockDevice.hpp"
@@ -29,40 +29,48 @@ struct CommandLineOptions
     std::string output_file_path;
 };
 
-static constexpr char USAGE[] =
-    R"(littlefs-extract.
 
-Usage:
-  littlefs-extract [-l VERSION] [-b BLOCK_SIZE] [-r READ_SIZE] [-p PROG_SIZE] -i INPUT [-o OUTPUT_FILE]
-  littlefs-extract -h | --help
-  littlefs-extract -h | --version
+namespace po = boost::program_options;
 
-Options:
-  -h --help                              Show this screen.
-  -v --version                           Show version.
-  -l VERSION --littlefs-version=VERSION  littlefs version to use. [default: 2]
-  -b BLOCK_SIZE --block-size=BLOCK_SIZE  filesystem block size. [default: 512]
-  -r READ_SIZE --read-size=READ_SIZE     filesystem read size. [default: 64]
-  -p PROG_SIZE --prog-size=PROG_SIZE     filesystem prog size. [default: 64]
-  -i INPUT --input-file=INPUT            littlefs image file.
-  -o OUTPUT --output-file=OUTPUT         output tar file. [default: -]
-)";
 
-CommandLineOptions parse_command_line(std::vector<std::string> const & argv)
+std::optional<CommandLineOptions> parse_command_line(std::vector<std::string> const & argv)
 {
-    auto args = docopt::docopt(USAGE,
-                               argv,
-                               true,
-                               "littlefs-extract v0.1");
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("version,v", "show version")
+        ("littlefs-version,l", po::value<std::uint32_t>()->default_value(2), "littlefs version to use")
+        ("block-size,b", po::value<std::uint32_t>()->default_value(512), "filesystem block size")
+        ("read-size,r", po::value<std::uint32_t>()->default_value(64), "filesystem read size")
+        ("prog-size,p", po::value<std::uint32_t>()->default_value(64), "filesystem prog size")
+        ("input-file,i", po::value<std::string>()->required(), "littlefs image file")
+        ("output-file,o", po::value<std::string>()->default_value("-"), "output tar file")
+    ;
+
+    po::variables_map vm {};
+    po::store(po::basic_command_line_parser(argv).options(desc).run(), vm);
+
+    if (vm.count("help"))
+    {
+        std::cout << desc << "\n";
+        return {};
+    }
+    if (vm.count("version"))
+    {
+        std::cout << "littlefs-extract v0.1\n";
+        return {};
+    }
+
+    po::notify(vm);
 
     CommandLineOptions options {};
 
-    options.version = boost::lexical_cast<std::uint32_t>(args.at("--littlefs-version").asString());
-    options.block_size = boost::lexical_cast<std::uint32_t>(args.at("--block-size").asString());
-    options.read_size = boost::lexical_cast<std::uint32_t>(args.at("--read-size").asString());
-    options.prog_size = boost::lexical_cast<std::uint32_t>(args.at("--prog-size").asString());
-    options.input_file_path = args.at("--input-file").asString();
-    options.output_file_path = args.at("--output-file").asString();
+    options.version = vm["littlefs-version"].as<std::uint32_t>();
+    options.block_size = vm["block-size"].as<std::uint32_t>();
+    options.read_size = vm["read-size"].as<std::uint32_t>();
+    options.prog_size = vm["prog-size"].as<std::uint32_t>();
+    options.input_file_path = vm["input-file"].as<std::string>();
+    options.output_file_path = vm["output-file"].as<std::string>();
 
     return options;
 }
@@ -87,23 +95,27 @@ int main(int argc, char ** argv)
 #endif
 
         auto const options = parse_command_line(arguments);
+        if (!options)
+        {
+            return 1;
+        }
 
-        auto const image_size = file_size(options.input_file_path);
-        if (image_size % options.block_size != 0)
+        auto const image_size = file_size(options->input_file_path);
+        if (image_size % options->block_size != 0)
         {
             throw std::runtime_error("Invalid block size");
         }
 
-        auto const block_count = image_size / options.block_size;
+        auto const block_count = image_size / options->block_size;
 
         if (block_count > std::numeric_limits<std::uint32_t>::max())
         {
             throw std::runtime_error("Image too large");
         }
 
-        auto image_file = std::make_unique<FileBlockDevice>(options.input_file_path,
+        auto image_file = std::make_unique<FileBlockDevice>(options->input_file_path,
                                                             false,
-                                                            options.block_size,
+                                                            options->block_size,
                                                             static_cast<std::uint32_t>(block_count));
     }
     catch (std::exception const & exception)
