439782b554601413a1eb0807bcbfee8f570669e1
Michael Bikovitsky <biko@bikodbg.com>
2020-03-13 17:22:57 +0200

    Implement utility for determining a file's size

---
+++ littlefs-extract/Util.cpp
@@ -0,0 +1,17 @@
+#include <filesystem>
+
+#if defined(_MSC_VER)
+#include "Unicode.hpp"
+#endif
+
+#include "Util.hpp"
+
+
+std::uintmax_t file_size(std::string const & path)
+{
+#if defined(_MSC_VER)
+    return std::filesystem::file_size(utf8_to_wide_char(path));
+#else
+    return std::filesystem::file_size(path);
+#endif
+}
---
+++ littlefs-extract/Util.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+
+std::uintmax_t file_size(std::string const & path);
