940d1e83f354541556cf193c5a33669edd614be1
Michael Bikovitsky <biko@bikodbg.com>
2020-03-20 13:42:45 +0200

    Fix file size check on macOS

--- littlefs-extract/Util.cpp
+++ littlefs-extract/Util.cpp
@@ -1,6 +1,15 @@
 #include "Util.hpp"
 
-#include <filesystem>
+#include <cerrno>
+#include <cstdint>
+#include <string>
+#include <system_error>
+
+#if __APPLE__
+    #include <sys/stat.h>
+#else
+    #include <filesystem>
+#endif
 
 #if defined(_MSC_VER)
     #include "Unicode.hpp"
@@ -11,6 +20,14 @@ std::uintmax_t file_size(std::string const & path)
 {
 #if defined(_MSC_VER)
     return std::filesystem::file_size(utf8_to_wide_char(path));
+#elif __APPLE__
+    struct stat stat_buffer = {};
+    auto const result = stat(path.c_str(), &stat_buffer);
+    if (-1 == result)
+    {
+        throw std::system_error(errno, std::system_category(), "stat");
+    }
+    return static_cast<uintmax_t>(stat_buffer.st_size);
 #else
     return std::filesystem::file_size(path);
 #endif
