038fc049af00ed03d28602a293a38a4a13f16bbb
Michael Bikovitsky <biko@bikodbg.com>
2020-03-21 21:09:06 +0200

    Fix obtaining a block device's size on *nix

--- littlefs-extract/Util.cpp
+++ littlefs-extract/Util.cpp
@@ -5,10 +5,14 @@
 #include <string>
 #include <system_error>
 
-#if __APPLE__
-    #include <sys/stat.h>
+#if defined(_MSC_VER)
+    #include <fcntl.h>
+    #include <io.h>
 #else
-    #include <filesystem>
+    #include <fcntl.h>
+    #include <sys/stat.h>
+    #include <sys/types.h>
+    #include <unistd.h>
 #endif
 
 #if defined(_MSC_VER)
@@ -16,19 +20,50 @@
 #endif
 
 
+#if defined(_MSC_VER)
+    #define lseek _lseeki64
+    #define close _close
+#endif
+
+
 std::uintmax_t file_size(std::string const & path)
 {
 #if defined(_MSC_VER)
-    return std::filesystem::file_size(utf8_to_wide_char(path));
-#elif __APPLE__
-    struct stat stat_buffer = {};
-    auto const result = stat(path.c_str(), &stat_buffer);
-    if (-1 == result)
+    int fd = -1;
+    auto const open_result = _wsopen_s(&fd,
+                                       utf8_to_wide_char(path).c_str(),
+                                       _O_RDONLY | _O_BINARY,
+                                       _SH_DENYNO,
+                                       _S_IREAD | _S_IWRITE);
+    if (0 != open_result)
     {
-        throw std::system_error(errno, std::system_category(), "stat");
+        throw std::system_error(errno, std::system_category(), "_wsopen_s");
     }
-    return static_cast<uintmax_t>(stat_buffer.st_size);
 #else
-    return std::filesystem::file_size(path);
+    auto const fd = open(path.c_str(), O_RDONLY);
+    if (-1 == fd)
+    {
+        throw std::system_error(errno, std::system_category(), "open");
+    }
 #endif
+
+    std::uintmax_t size = 0;
+
+    try
+    {
+        auto const lseek_result = lseek(fd, 0, SEEK_END);
+        if (-1 == lseek_result)
+        {
+            throw std::system_error(errno, std::system_category(), "lseek");
+        }
+        size = static_cast<std::uintmax_t>(lseek_result);
+    }
+    catch (...)
+    {
+        close(fd);
+        throw;
+    }
+    close(fd);
+
+    return size;
 }
